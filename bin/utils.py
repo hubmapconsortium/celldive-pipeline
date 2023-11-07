380451d2ff3aac056b3527d00df50e9fbef76f07
Matt Ruffalo <matt.ruffalo@gmail.com>
2023-11-07 12:25:24 -0500

    Support wildcards for segmentation channels

--- bin/utils.py
+++ bin/utils.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict
+from typing import List, Tuple
 
 import yaml
 
@@ -28,13 +28,13 @@ def save_pipeline_config(config: dict, out_path: Path):
         yaml.safe_dump(config, s)
 
 
-def get_channel_names_from_ome(xml) -> Dict[str, int]:
+def get_channel_names_from_ome(xml) -> List[Tuple[str, int]]:
     pixels = xml.find("Image").find("Pixels")
     channels = pixels.findall("Channel")
-    ch_names_ids = dict()
+    ch_names_ids = []
     for ch in channels:
         ch_name = ch.get("Name")
         ch_id_ome = ch.get("ID")  # e.g. Channel:0:12
         ch_id = int(ch_id_ome.split(":")[-1])
-        ch_names_ids[ch_name] = ch_id
+        ch_names_ids.append((ch_name, ch_id))
     return ch_names_ids
